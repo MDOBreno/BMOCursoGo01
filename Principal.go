f850c3c039083c657d1d591007d7e916b7d13c2e
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2021-05-10 16:55:34 -0300

    Aula 08 Concluida

--- Principal.go
+++ Principal.go
@@ -5,5 +5,26 @@ import (
 )
 
 func main() {
+	// aqui começa
+
+	//[Video 07]
+	//pacote.identificador("Hello, World!")
 	fmt.Println("Hello, World!")
+
+	//[Video 08]
+	numeroDeBytes, erros := fmt.Println("Hello, World!", "Oi galera!", 100)
+	fmt.Println(numeroDeBytes, erros) //Como GO não permite a instanciacao de variaveis que não estejam sendo usadas estou "printando-os"
+
+	// Outra forma de lidar com isso quando queremos apenas usar um dos valores retornados é usar o '_' dessa forma:
+	_, numErros := fmt.Println("Hello, World!", "Oi galera!", 100)
+	fmt.Println(numErros)
+
+	x := 16
+	y := "string"
+	z := true
+	fmt.Println(x, y, z)
+
+	//[Video 09]
+
+	// aqui termina
 }
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Inclui o conteudo dos videos:
 
 07) Cap 2 - Variaveis, Valores e Tipos
+08) Cap 2 – Variáveis, Valores & Tipos – 2. Hello world!
